98201b8620efadd8f1385098b3fd54c9be49dc29
pikuch <55052790+pikuch@users.noreply.github.com>
2021-11-26 19:06:13 +0100

    Add generic repository

---
+++ MusicApiData/DAL/IMusicApiRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApiData.DAL
+{
+    interface IMusicApiRepository<TModel> where TModel : class
+    {
+        ICollection<TModel> GetAll();
+        TModel GetById(long id);
+        void Insert(TModel model);
+        void Update(TModel model);
+        void Delete(long id);
+    }
+}
---
+++ MusicApiData/DAL/MusicApiRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApiData.DAL
+{
+    class MusicApiRepository<TModel> : IMusicApiRepository<TModel> where TModel : class
+    {
+        private readonly MusicApiDbContext _context = null;
+        private readonly DbSet<TModel> _table = null;
+
+        public MusicApiRepository(MusicApiDbContext context)
+        {
+            _context = context;
+            _table = _context.Set<TModel>();
+        }
+        public ICollection<TModel> GetAll()
+        {
+            return _table.ToList();
+        }
+
+        public TModel GetById(long id)
+        {
+            return _table.Find(id);
+        }
+
+        public void Insert(TModel model)
+        {
+            _table.Add(model);
+            _context.SaveChanges();
+        }
+
+        public void Update(TModel model)
+        {
+            _table.Attach(model);
+            _context.Entry(model).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Delete(long id)
+        {
+            _table.Remove(_table.Find(id));
+            _context.SaveChanges();
+        }
+
+    }
+}
